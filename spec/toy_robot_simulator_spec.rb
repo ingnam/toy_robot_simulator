4975c01cb4646734c43e6859f61a1436051e5e66
Madan Ingnam <madingnm@gmail.com>
2022-07-20 20:34:41 +1000

    initial commit

---
+++ spec/toy_robot_simulator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ToyRobotSimulator do
+  it "has a version number" do
+    expect(ToyRobotSimulator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
